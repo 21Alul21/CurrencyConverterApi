57a4b35fdafe5473150eaab1cd21f26e83c08b3e
21Alul21 <augustine.alul@yahoo.com>
2025-09-18 10:42:18 +0100

    seerated the various external APIs to different service files

--- src/main/java/com/api/currencyconverterservice/service/OpenExchangeApiService.java
+++ src/main/java/com/api/currencyconverterservice/service/OpenExchangeApiService.java
@@ -1,5 +1,24 @@
 package com.api.currencyconverterservice.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
 public class OpenExchangeApiService {
 
+    private final WebClient webClient;
+
+    public OpenExchangeApiService(WebClient webClient){
+        this.webClient = webClient;
+    }
+
+    public String getCurrencies(){
+        String url = "https://openexchangerates.org/api/currencies.json?app_id=d59906d5f9e142bcb36f75146ef54ec5";
+        return webClient
+        .get()
+        .uri(url)
+        .retrieve()
+        .bodyToMono(String.class)
+        .block();
+    }
 }
