fb98db6d62a333d6b286670c6e7f41aa532e5054
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-10-02 22:09:42 +0100

    Update CurrencyconverterserviceApplication.java

--- src/main/java/com/api/currencyconverterservice/CurrencyconverterserviceApplication.java
+++ src/main/java/com/api/currencyconverterservice/CurrencyconverterserviceApplication.java
@@ -2,6 +2,8 @@ package com.api.currencyconverterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @EnableCaching
 @SpringBootApplication
